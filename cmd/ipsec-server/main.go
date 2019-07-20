1689074b6e57fd357a6b91ffaa93d93a949a92f3
Bogdan Ionescu <14040788+BogdanIonesq@users.noreply.github.com>
2019-07-20 20:12:59 +0300

    goroutines and encrypt/decrypt funcs

--- cmd/ipsec-server/main.go
+++ cmd/ipsec-server/main.go
@@ -9,6 +9,8 @@ import (
 )
 
 const (
+	LocalGatewayMAC              = "08:00:27:ad:c3:95"
+	RemoteGatewayMAC             = "08:00:27:11:27:9f"
 	LocalGatewayAddr             = "fe80::f14:e938:f365:86bf"
 	RemoteGatewayAddr            = "fe80::6e42:c109:74ed:6dab"
 	NetworkLayerDataOffset       = 14
@@ -17,38 +19,50 @@ const (
 )
 
 func main() {
-	go listenInside()
-	//listenPackets()
+	listen()
 }
 
-func listenInside() {
+func listen() {
 	handle, err := pcap.OpenLive("enp0s3", 1600, true, pcap.BlockForever)
 	if err != nil {
 		panic(err)
 	}
 
+	// sniff only UDP/ESP traffic for now
 	err = handle.SetBPFFilter("udp or esp")
 	if err != nil {
 		panic(err)
 	}
 
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	// channels
+	send := make(chan gopacket.SerializeBuffer)
+	recv := gopacket.NewPacketSource(handle, handle.LinkType()).Packets()
 
-	for packet := range packetSource.Packets() {
-		fmt.Printf("Packet from %s to %s",
-			net.IPv4(packet.Data()[26], packet.Data()[27], packet.Data()[28], packet.Data()[29]).String(),
-			net.IPv4(packet.Data()[30], packet.Data()[31], packet.Data()[32], packet.Data()[33]).String())
-		go encryptPacket(packet)
+	select {
+	case packet := <-recv:
+		if packet.Layer(layers.LayerTypeUDP) != nil {
+			go encryptPacket(packet, send)
+		} else if packet.Layer(layers.LayerTypeIPSecESP) != nil {
+			go decryptPacket(packet, send)
+		}
+	case packet := <-send:
+		err := handle.WritePacketData(packet.Bytes())
+
+		if err != nil {
+			fmt.Println("Send packet error: ", err)
+		}
 	}
 }
 
-func encryptPacket(packet gopacket.Packet) {
-	encryptedPacket := gopacket.NewSerializeBuffer()
+func encryptPacket(packet gopacket.Packet, send chan gopacket.SerializeBuffer) {
+	srcMAC, _ := net.ParseMAC(LocalGatewayMAC)
+	dstMAC, _ := net.ParseMAC(RemoteGatewayMAC)
 
+	encryptedPacket := gopacket.NewSerializeBuffer()
 	err := gopacket.SerializeLayers(encryptedPacket, gopacket.SerializeOptions{},
 		&layers.Ethernet{
-			SrcMAC:       net.HardwareAddr{0x88, 0xb1, 0x11, 0x61, 0x79, 0x7f},
-			DstMAC:       net.HardwareAddr{0x08, 0x00, 0x27, 0x11, 0x27, 0x9f},
+			SrcMAC:       srcMAC,
+			DstMAC:       dstMAC,
 			EthernetType: layers.EthernetTypeIPv6,
 		},
 		&layers.IPv6{
@@ -70,105 +84,16 @@ func encryptPacket(packet gopacket.Packet) {
 
 	if err != nil {
 		fmt.Println("Packet creation error: ", err)
+	} else {
+		send <- encryptedPacket
 	}
 }
 
-func listen() {
-	handle, err := pcap.OpenLive("enp0s3", 1600, true, pcap.BlockForever)
-	if err != nil {
-		panic(err)
-	}
-
-	err = handle.SetBPFFilter("esp")
-	if err != nil {
-		panic(err)
-	}
-
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	if handle, err := pcap.OpenLive("enp0s3", 1600, true, pcap.BlockForever); err != nil {
-		panic(err)
-	} else if err := handle.SetBPFFilter("esp"); err != nil {
-		panic(err)
-	} else {
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		for packet := range packetSource.Packets() {
-			fmt.Println(packet)
-
-			fmt.Println("writing!")
-
-			forwardPacket := gopacket.NewSerializeBuffer()
-			err := gopacket.SerializeLayers(forwardPacket, gopacket.SerializeOptions{},
-				&layers.Ethernet{
-					SrcMAC:       net.HardwareAddr{0x88, 0xb1, 0x11, 0x61, 0x79, 0x7f},
-					DstMAC:       net.HardwareAddr{0x08, 0x00, 0x27, 0x11, 0x27, 0x9f},
-					EthernetType: layers.EthernetTypeIPv6,
-				},
-				gopacket.Payload(packet.Data()[62:]),
-			)
-
-			if err != nil {
-				fmt.Println("Packet creation error: ", err)
-			}
-
-			_ = handle.WritePacketData(forwardPacket.Bytes())
-		}
-	}
-}
-
-func sendPacket() {
-	var packet gopacket.Packet
-
-	if handle, err := pcap.OpenOffline("/home/bogdan/bsc-thesis/pcap-files/IPv6-ping.pcap"); err != nil {
-		panic(err)
-	} else {
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-
-		packet = <-packetSource.Packets()
-	}
-
-	data := packet.Data()[14:]
-
-	ipsecPacket := gopacket.NewSerializeBuffer()
-	err := gopacket.SerializeLayers(ipsecPacket, gopacket.SerializeOptions{},
-		&layers.Ethernet{
-			SrcMAC:       net.HardwareAddr{0x88, 0xb1, 0x11, 0x61, 0x79, 0x7f},
-			DstMAC:       net.HardwareAddr{0x08, 0x00, 0x27, 0x11, 0x27, 0x9f},
-			EthernetType: layers.EthernetTypeIPv6,
-		},
-		&layers.IPv6{
-			Version:      6,
-			TrafficClass: 0,
-			FlowLabel:    0,
-			Length:       8 + uint16(len(data)),
-			NextHeader:   layers.IPProtocolESP,
-			HopLimit:     64,
-			SrcIP:        net.ParseIP("fe80::f14:e938:f365:86bf"),
-			DstIP:        net.ParseIP("fe80::6e42:c109:74ed:6dab"),
-		},
-		// SPI
-		gopacket.Payload([]byte{1, 2, 3, 4}),
-		// Sequence Number
-		gopacket.Payload([]byte{1, 2, 3, 4}),
-		gopacket.Payload(data),
+func decryptPacket(packet gopacket.Packet, send chan gopacket.SerializeBuffer) {
+	decryptedPacket := gopacket.NewSerializeBuffer()
+	_ = gopacket.SerializeLayers(decryptedPacket, gopacket.SerializeOptions{},
+		gopacket.Payload(packet.Data()[TransportLayerDataOffsetIPv6:]),
 	)
 
-	if err != nil {
-		fmt.Println("Packet creation error: ", err)
-	}
-
-	// write to wire
-	handle, err := pcap.OpenLive("wlp4s0", 1024, false, pcap.BlockForever)
-
-	if err != nil {
-		fmt.Println("pcap open error: ", err)
-	}
-	defer handle.Close()
-
-	err = handle.WritePacketData(ipsecPacket.Bytes())
-
-	if err != nil {
-		fmt.Println("Send packet error: ", err)
-	}
-
-	fmt.Println("done!")
+	send <- decryptedPacket
 }
